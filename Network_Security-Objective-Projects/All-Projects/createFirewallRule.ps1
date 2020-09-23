80fdff59866c1fe273477edcda5b4ac71572d2f5
Jose Ramirez <43449764+JosRami@users.noreply.github.com>
2020-09-22 21:09:02 -0600

    Added objective 3 files

---
+++ Network_Security-Objective-Projects/All-Projects/createFirewallRule.ps1
@@ -0,0 +1,6 @@
+﻿$name = Read-Host -Prompt 'Give the rule a name: '
+$direction = Read-Host -Prompt 'Inbound or Outbound: '
+$port = Read-Host -Prompt 'Enter a Port you want to open: '
+$option = Read-Host -Prompt 'Allow or Block: '
+
+New-NetFirewallRule -DisplayName $name -Direction $direction -LocalPort $port -Protocol TCP -Action $option
